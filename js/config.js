ee75fcaca05fee0d785753b74fadb4d812b34543
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-07-18 22:35:10 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
 // js/config.js
 const CONFIG = {
     APP_VERSION: '0.0.0.0.1 ',
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbyzPRbsSuPhSTfq5MdQJSeLWd96ijgYm-Ry0nVwrWa8S4RNmWT18VGf3BvtKicZ6UtX/exec'
-};
+    };
