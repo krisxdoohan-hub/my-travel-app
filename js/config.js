8789f2839b6040b5844f8902cbed9b970508a293
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-07-20 00:13:11 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 const CONFIG = {
-    APP_VERSION: '0.0.0.0.8 ',
+    APP_VERSION: '0.0.0.0.9 ',
     GAS_URL: 'https://script.google.com/macros/s/AKfycbyzPRbsSuPhSTfq5MdQJSeLWd96ijgYm-Ry0nVwrWa8S4RNmWT18VGf3BvtKicZ6UtX/exec' // 請將您部署好的 Web App URL 填入這裡，或直接透過前端 UI 介面填寫
 };
