b2ce92e71dadd2634dd4e1dbe5cb0dcbefabefa6
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-07-27 02:26:35 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 const CONFIG = {
-    APP_VERSION: '0.0.0.0.15 - 卡片顏色區別',
+    APP_VERSION: '0.0.0.1.6 - 天氣功能強化 ',
     GAS_URL: 'https://script.google.com/macros/s/AKfycbyzPRbsSuPhSTfq5MdQJSeLWd96ijgYm-Ry0nVwrWa8S4RNmWT18VGf3BvtKicZ6UtX/exec' // 請將您部署好的 Web App URL 填入這裡，或直接透過前端 UI 介面填寫
 };
