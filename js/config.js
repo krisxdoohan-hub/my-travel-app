cb5e42432f98fd765184562148e14110a2105032
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-08-01 16:15:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 const CONFIG = {
-    APP_VERSION: '0.0.0.2.2 - 修正權限設定 ',
+    APP_VERSION: '0.0.0.2.3 - 修正清單設定 ',
     GAS_URL: 'https://script.google.com/macros/s/AKfycbyzPRbsSuPhSTfq5MdQJSeLWd96ijgYm-Ry0nVwrWa8S4RNmWT18VGf3BvtKicZ6UtX/exec' // 請將您部署好的 Web App URL 填入這裡，或直接透過前端 UI 介面填寫
 };
